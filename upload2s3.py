bc9686f34d8b8aebae2c5ad381866858b83817a1
yang zhou <bnm965321@gmail.com>
2018-08-27 07:11:48 +0800

    update s3upload scripts

--- upload2s3.py
+++ upload2s3.py
@@ -9,7 +9,7 @@ from scripts.mongo2s3 import main
 logging.basicConfig(level=logging.INFO)
 
 loop = asyncio.get_event_loop()
-start, end = arrow.utcnow().shift(weeks=-1), arrow.utcnow()
+start, end = arrow.utcnow().shift(weeks=-1), arrow.utcnow().shift(days=-1)
 loop.run_until_complete(main(start, end))
 
 
