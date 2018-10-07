8c6231a7476b58c6f08ec6f1138b06849b5448db
yang zhou <bnm965321@gmail.com>
2018-10-08 02:52:24 +0800

    add crontab from upload script

--- upload2s3.py
+++ upload2s3.py
@@ -2,14 +2,26 @@
 author: thomaszdxsn
 """
 import asyncio
-import arrow
 import logging
+
+import arrow
+import pytz
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 from scripts.mongo2s3 import main
 
 logging.basicConfig(level=logging.INFO)
 
-loop = asyncio.get_event_loop()
-start, end = arrow.utcnow().shift(weeks=-1), arrow.utcnow()
-loop.run_until_complete(main(start, end))
+
+def task():
+    loop = asyncio.get_event_loop()
+    start, end = arrow.utcnow().shift(weeks=-1), arrow.utcnow()
+    loop.run_until_complete(main(start, end))
+
+
+scheduler = BlockingScheduler(timezone=pytz.UTC)
+scheduler.add_job(task, trigger='cron', hour=0, minute=15)
+print('starting scheduler')
+scheduler.start()
 
 
